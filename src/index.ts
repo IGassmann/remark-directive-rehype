d1d661c48406a84643ca3de0b5466abbbc312edd
Igor Gassmann <igor@igassmann.me>
2021-11-12 15:32:33 +0300

    feat: implement remark plugin

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello')
+import { h } from 'hastscript'
+import { map } from 'unist-util-map'
+
+import type { Directive as DirectiveNode } from 'mdast-util-directive';
+import type { Plugin, Transformer } from 'unified';
+import type { Node, MapFunction } from 'unist-util-map'
+
+const isDirectiveNode = (node: Node): node is DirectiveNode => {
+  const { type } = node;
+  return type === 'textDirective' || type === 'leafDirective' || type === 'containerDirective';
+}
+
+const mapDirectiveNode: MapFunction = (node) => {
+  if (isDirectiveNode(node)) {
+
+    const { properties, tagName } = h(node.name, node.attributes);
+
+    return {
+      ...node,
+      data: {
+        hName: tagName,
+        hProperties: properties
+      }
+    }
+  }
+
+  return node;
+};
+
+const transformNodeTree: Transformer = (nodeTree) => map(nodeTree, mapDirectiveNode);
+
+const remarkDirectiveRehype: Plugin = () => transformNodeTree;
+
+export default remarkDirectiveRehype;
